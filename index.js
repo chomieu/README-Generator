4cd57f381e548612fa5a9713bebb9934d632c036
chomieu <chomieu@gmail.com>
2021-01-08 02:08:40 -0800

    Sucessfully wrote and executed inquirer prompts

--- index.js
+++ index.js
@@ -1,3 +1,53 @@
 const inquirer = require("inquirer")
 const fs = require("fs")
 
+inquirer.prompt([
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of the project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "What is the description of the project?"
+    },
+    {
+        type: "input",
+        name: "table of contents",
+        message: "What is the title of the project?"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What is the description of the project?"
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of the project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "What is the description of the project?"
+    },
+    {
+        type: "input",
+        name: "table of contents",
+        message: "What is the title of the project?"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What is the description of the project?"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What is the description of the project?"
+    }
+]).then(answer => {
+    fs.writeFile("README.md", JSON.stringify(answer), (err) =>
+        err ? console.error(err) : console.log("success"))
+})
