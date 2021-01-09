b967074e9816d0657a3b7ffd33defeecebd47e5e
chomieu <chomieu@gmail.com>
2021-01-08 23:19:07 -0800

    Revised written code to work with the original template

---
+++ generateMarkdown.js
@@ -0,0 +1,61 @@
+// Creates a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  switch (license) {
+    case "MIT":
+      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]"
+      break
+    case "GNU GPLv3":
+      return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]"
+      break
+    case "Unlicense":
+      return "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)]"
+      break
+    default:
+      return ""
+  }
+}
+
+// Creates a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  switch (license) {
+    case "MIT":
+      return "(https://choosealicense.com/licenses/mit)"
+      break
+    case "GNU GPLv3":
+      return "(https://choosealicense.com/licenses/gpl-3.0)"
+      break
+    case "Unlicense":
+      return "(https://choosealicense.com/licenses/unlicense)"
+      break
+    default:
+      return ""
+  }
+}
+
+// Creates a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  if (license === "No license") {
+    return ""
+  } else {
+    return `\n\n## License\n[${license}]${renderLicenseLink(license)}`
+  }
+}
+
+// Creates a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}\n${renderLicenseBadge(data.license) + renderLicenseLink(data.license)}
+  \n## Description\n${data.description}
+  \n## Table of Contents\n* [Installation](#installation)\n* [Usage](#usage)\n* [Contributing](#contributing)\n* [Tests](#tests)\n* [License](#license)\n* [Questions](#questions)
+  \n## Installation\n${data.installation} 
+  \n## Usage\n${data.usage}
+  \n## Contributing\n${data.contributing}
+  \n## Tests\n${data.tests}`
+  + renderLicenseSection(data.license) +
+  `\n\n## Questions\nFor questions, contact github.com/${data.username} or email ${data.email}`
+}
+
+// Exports generateMarkdown function
+module.exports = {generateMarkdown};
--- index.js
+++ index.js
@@ -1,84 +1,68 @@
-// Calls the inquirer and fs packages
+// Includes packages needed for this application
+const generate = require("./generateMarkdown.js")
 const inquirer = require("inquirer")
 const fs = require("fs")
 
-// Prompts for user input then processes the response
-inquirer.prompt([
-    {
-        type: "input",
-        name: "title",
-        message: "Project Title:"
-    },
-    {
-        type: "input",
-        name: "description",
-        message: "Description:"
-    },
-    {
-        type: "input",
-        name: "installation",
-        message: "Installation Instructions:"
-    },
-    {
-        type: "input",
-        name: "usage",
-        message: "Usage Information:"
-    },
-    {
-        type: "input",
-        name: "contributing",
-        message: "Contribution Guidelines:"
-    },
-    {
-        type: "input",
-        name: "tests",
-        message: "Test Instructions:"
-    },
-    {
-        type: "list",
-        name: "license",
-        message: "Choose a License:",
-        choices: ["MIT", "GPL-3.0", "Unlicense"]
-    },
-    {
-        type: "input",
-        name: "username",
-        message: "Enter your GitHub username:"
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "Enter your email address:"
-    }
-]).then(response => {
+// Creates an array of questions for user input
+const questions = [{
+    type: "input",
+    name: "title",
+    message: "Project Title:"
+},
+{
+    type: "input",
+    name: "description",
+    message: "Description:"
+},
+{
+    type: "input",
+    name: "installation",
+    message: "Installation Instructions:"
+},
+{
+    type: "input",
+    name: "usage",
+    message: "Usage Information:"
+},
+{
+    type: "input",
+    name: "contributing",
+    message: "Contribution Guidelines:"
+},
+{
+    type: "input",
+    name: "tests",
+    message: "Test Instructions:"
+},
+{
+    type: "list",
+    name: "license",
+    message: "Choose a License:",
+    choices: ["MIT", "GNU GPLv3", "Unlicense", "No license"]
+},
+{
+    type: "input",
+    name: "username",
+    message: "Enter your GitHub username:"
+},
+{
+    type: "input",
+    name: "email",
+    message: "Enter your email address:"
+}];
 
-    // Returns matching badge
-    function getBadge(license) {
-        switch (license) {
-            case "MIT":
-                return "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)"
-                break
-            case "GPL-3.0":
-                return "![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)"
-                break
-            case "Unlicense":
-                return "![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)"
-        }
-    }
-
-    // Template gets filled based on the user's response to each question
-    const template =
-        `# ${response.title}\n${getBadge(response.license)}
-        \n## Description\n${response.description}
-        \n## Table of Contents\n* [Installation](#installation)\n* [Usage](#usage)\n* [Contributing](#contributing)\n* [Tests](#tests)\n* [License](#license)\n* [Questions](#questions)
-        \n## Installation\n${response.installation} 
-        \n## Usage\n${response.usage}
-        \n## Contributing\n${response.contributing}
-        \n## Tests\n${response.tests}
-        \n## License\n[${response.license}](https://choosealicense.com/licenses/${response.license.toLowerCase()}/)
-        \n## Questions\nFor questions, contact github.com/${response.username} or email ${response.email}`
-
-    // Creates the README file and log the error or success massage to the console 
-    fs.writeFile("README.md", template, (err) =>
+// Creates a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generate.generateMarkdown(data), (err) =>
         err ? console.error(err) : console.log("success"))
-})
+}
+
+// Creates a function to initialize app
+function init() {
+    inquirer.prompt(questions).then(response => {
+        writeToFile("README.md", response)
+    })
+}
+
+// Function call to initialize app
+init();
